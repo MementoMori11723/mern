eb671a3188901a62fa07bba38102e8ad3248e74e
yasasvi <yasasvigumma@gmail.com>
2024-03-24 17:45:11 +0530

    started a small server to build backend which connects to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
+
+const port = process.env.PORT || 5000;
+mongoose
+  .connect(
+    "mongodb+srv://yasasvigumma:pt1w4gOo9PwugUCT@merlin.y6yvre0.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(port, () => {
+      console.log(`App lising at http://localhost:${port}`);
+    });
+  });
