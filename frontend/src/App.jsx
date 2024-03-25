07f35f372f574f817ff6a7507b881e1c34d4f2f9
yasasvi <yasasvigumma@gmail.com>
2024-03-25 20:07:05 +0530

    making ui/ux design for E-Cart application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-// import { useState } from "react";
 import { Home, About } from "./components/import";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div className="text-center">
       <BrowserRouter>
