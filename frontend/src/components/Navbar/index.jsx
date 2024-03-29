513c08c244b8af1927599a3e4e19d774fe3054c7
yasasvi <yasasvigumma@gmail.com>
2024-03-29 21:39:26 +0530

    changing the image source

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -7,11 +7,7 @@ const Navbar = () => {
       <div className="container hidden lg:block">
         <div className="flex justify-between items-center p-8">
           <div className="flex justify-between items-center">
-            <img
-              alt="logo"
-              className="mr-2 w-8 h-8"
-              src="data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGRhdGEtbmFtZT0iTGF5ZXIgMSIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxnIGRhdGEtbmFtZT0iJmx0O0dyb3VwJmd0OyI+PHBhdGggZmlsbD0iIzAwZWZkMSIgZD0iTTQ2Mi4xLDEyMS45LDQyNC44LDI0MC40YTUwLjAyOCw1MC4wMjgsMCwwLDEtNDcuNywzNUgxNDEuNmwtLjUuMUwxMTUsMTIybC40LS4xWiIvPjxwYXRoIGZpbGw9IiMwMGFjZWEiIGQ9Ik0zOTMuMSA0MDIuMWE0MC44IDQwLjggMCAxIDEtNDAuOCA0MC44QTQwLjg0MyA0MC44NDMgMCAwIDEgMzkzLjEgNDAyLjF6TTE3OS4yIDQwMi4xYTQwLjggNDAuOCAwIDEgMS00MC44IDQwLjhBNDAuNzc5IDQwLjc3OSAwIDAgMSAxNzkuMiA0MDIuMXpNNDIzLjkgMzU3LjhIMTk1LjdhNTkuOTE4IDU5LjkxOCAwIDAgMS01OS4yLTUwTDk2LjQgNzEuNkEzOS45MjkgMzkuOTI5IDAgMCAwIDU3IDM4LjNIMzkuOWExMCAxMCAwIDAgMSAwLTIwSDU3YTU5LjczNyA1OS43MzcgMCAwIDEgNTkuMSA1MGw0MC4xIDIzNi4zYTM5LjkyOSAzOS45MjkgMCAwIDAgMzkuNCAzMy4zSDQyMy44YTEwLjAyOSAxMC4wMjkgMCAwIDEgMTAgMTBBOS44NTkgOS44NTkgMCAwIDEgNDIzLjkgMzU3Ljh6Ii8+PC9nPjwvc3ZnPg=="
-            />
+            <img alt="logo" className="mr-2 w-8 h-8" src="/image.svg" />
             <h1 className="text-2xl font-medium">Logo</h1>
           </div>
           <div className="relative w-full max-w-[500px]">
